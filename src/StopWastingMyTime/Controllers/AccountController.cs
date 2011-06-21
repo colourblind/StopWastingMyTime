cc552a247d656a7901c74af7097d574aef5158c2
colourblind <tom.milsom@gmail.com>
2011-06-21 07:06:31 +0100

    Knew I'd forgotten something . . .

---
+++ src/StopWastingMyTime/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StopWastingMyTime.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Login(FormCollection form)
+        {
+            Models.User user = Models.User.Validate(form["username"], form["password"]);
+            if (user != null)
+                FormsAuth
+            
+            return View();
+        }
+    }
+}
