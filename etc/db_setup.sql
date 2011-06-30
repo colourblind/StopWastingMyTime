28110a2d769d714159814d1c5f3de8b84fffb72f
colourblind <tom.milsom@gmail.com>
2011-06-30 18:28:43 +0100

    Updated DB creation script

--- etc/db_setup.sql
+++ etc/db_setup.sql
@@ -5,6 +5,16 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+CREATE TABLE [dbo].[Client](
+	[ClientId] [uniqueidentifier] NOT NULL CONSTRAINT [DF_Client_ClientId]  DEFAULT (newid()),
+	[Name] [nvarchar](100) NOT NULL,
+ CONSTRAINT [PK_Client] PRIMARY KEY CLUSTERED 
+(
+	[ClientId] ASC
+)WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 CREATE TABLE [dbo].[User](
 	[UserId] [nvarchar](50) NOT NULL,
 	[Password] [nvarchar](50) NOT NULL,
@@ -18,13 +28,19 @@ GO
 
 CREATE TABLE [dbo].[Job](
 	[JobId] [nvarchar](50) NOT NULL,
+	[ClientId] [uniqueidentifier] NOT NULL,
 	[Billable] [bit] NOT NULL,
  CONSTRAINT [PK_Job] PRIMARY KEY CLUSTERED 
 (
 	[JobId] ASC
 )WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+
 GO
+ALTER TABLE [dbo].[Job]  WITH CHECK ADD  CONSTRAINT [FK_Job_Client] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Client] ([ClientId])
+GO
+ALTER TABLE [dbo].[Job] CHECK CONSTRAINT [FK_Job_Client]
 
 CREATE TABLE [dbo].[TimeBlock](
 	[TimeBlockId] [uniqueidentifier] NOT NULL CONSTRAINT [DF_TimeBlock_TimeBlockId]  DEFAULT (newid()),
