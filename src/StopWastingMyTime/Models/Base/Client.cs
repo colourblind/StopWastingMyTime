6bd7027b448377918cc72d183781d241fe7df51e
colourblind <tom.milsom@gmail.com>
2011-07-01 18:19:19 +0100

    Added maintenance field to clients (UI to follow)

--- src/StopWastingMyTime/Models/Base/Client.cs
+++ src/StopWastingMyTime/Models/Base/Client.cs
@@ -19,6 +19,7 @@ namespace StopWastingMyTime.Models.Base
 
         private Guid _clientId = Guid.Empty;
         private string _name = String.Empty;
+        private decimal _maintenancePerMonth;
 
 
         private List<StopWastingMyTime.Models.Job> _jobs = null;
@@ -39,6 +40,12 @@ namespace StopWastingMyTime.Models.Base
         {
             get { return _name; }
             set { _name = value; }
+        }
+		
+        public virtual decimal MaintenancePerMonth
+        {
+            get { return _maintenancePerMonth; }
+            set { _maintenancePerMonth = value; }
         }
 		
 
@@ -94,6 +101,7 @@ namespace StopWastingMyTime.Models.Base
             {
                 _clientId = dataObject.ClientId;
                 _name = dataObject.Name;
+                _maintenancePerMonth = dataObject.MaintenancePerMonth;
 
                 _isNew = false;
             }
@@ -112,6 +120,7 @@ namespace StopWastingMyTime.Models.Base
 
             dataObject.ClientId = _clientId;
             dataObject.Name = _name;
+            dataObject.MaintenancePerMonth = _maintenancePerMonth;
 
             if (IsNew)
                 dataObject.Insert();
