7d26db68e3312067ad3d322c8442d089f8e065b0
colourblind <tom.milsom@gmail.com>
2011-07-14 19:23:13 +0100

    Maintenance report now includes clients with no time against them for the month

--- src/StopWastingMyTime/Models/Data/Reporting.cs
+++ src/StopWastingMyTime/Models/Data/Reporting.cs
@@ -70,15 +70,20 @@ namespace StopWastingMyTime.Models.Data
         private const string MAINTENANCE_SQL =
 @"
 SELECT
-    c.[Name] AS [Client], c.MaintenancePerMonth AS [Maintenance per Month], SUM(t.[Time]) AS [Total Hours], CASE WHEN SUM(t.[Time]) > c.MaintenancePerMonth THEN 1 ELSE 0 END AS [Overrun]
+    c.[Name] AS [Client], 
+    c.MaintenancePerMonth AS [Maintenance per Month], 
+    COALESCE(SUM(t.[Time]), 0) AS [Total Hours], 
+    CONVERT(bit, CASE WHEN SUM(t.[Time]) > c.MaintenancePerMonth THEN 1 ELSE 0 END) AS [Overrun]
 FROM
-    [TimeBlock] t 
-    INNER JOIN [Job] j ON t.JobId = j.JobId
-    INNER JOIN [Client] c ON j.ClientId = c.ClientId
+    [Client] c
+    LEFT OUTER JOIN [Job] j ON c.ClientId = j.ClientId
+    LEFT OUTER JOIN [TimeBlock] t ON j.JobId = t.JobId
 WHERE
-    j.Billable = 0
-    AND t.[Date] >= DATEADD(dd, DATEDIFF(dd, 0, DATEADD(mm, 0, DATEADD(dd, -DAY(@Date) + 1, @Date))), 0)
-    AND t.[Date] < DATEADD(dd, DATEDIFF(dd, 0, DATEADD(mm, 1, DATEADD(dd, -DAY(@Date) + 1, @Date))), 0)
+    j.Billable IS NULL
+    OR t.[Date] IS NULL
+    OR j.Billable = 0
+    AND t.[Date] >= DATEADD(dd, DATEDIFF(dd, 0, DATEADD(mm, 0, DATEADD(dd, -DAY(getDate()) + 1, getDate()))), 0)
+    AND t.[Date] < DATEADD(dd, DATEDIFF(dd, 0, DATEADD(mm, 1, DATEADD(dd, -DAY(getDate()) + 1, getDate()))), 0)
 GROUP BY
     DATEPART(yy, t.Date), DATEPART(mm, t.Date), c.Name, c.MaintenancePerMonth
 ";
@@ -86,7 +91,10 @@ GROUP BY
         private const string MONTHLY_SQL =
 @"
 SELECT
-    j.JobId AS [Job], j.QuotedHours AS [Quoted Hours], j.Billable AS [Billable], SUM(t.[Time]) AS [Total Hours], CASE WHEN j.QuotedHours IS NOT NULL AND SUM(t.[Time]) > j.QuotedHours THEN 1 ELSE 0 END AS [Overrun]
+    j.JobId AS [Job], 
+    j.QuotedHours AS [Quoted Hours], 
+    j.Billable AS [Billable], SUM(t.[Time]) AS [Total Hours], 
+    CONVERT(bit, CASE WHEN j.QuotedHours IS NOT NULL AND SUM(t.[Time]) > j.QuotedHours THEN 1 ELSE 0 END) AS [Overrun]
 FROM
     [TimeBlock] t
     INNER JOIN [Job] j ON t.JobId = j.JobId
