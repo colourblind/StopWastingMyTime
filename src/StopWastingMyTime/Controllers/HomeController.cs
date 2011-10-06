c598bfed9b7f8e58af65757b00a717c8f5ef5043
colourblind <tom.milsom@gmail.com>
2011-10-06 17:15:10 +0100

    Missing from last commit

--- src/StopWastingMyTime/Controllers/HomeController.cs
+++ src/StopWastingMyTime/Controllers/HomeController.cs
@@ -11,7 +11,21 @@ namespace StopWastingMyTime.Controllers
     {
         public ActionResult Index()
         {
-            ViewData["MonthlyReport"] = Models.Data.Reporting.MonthlyReport();
+            DateTime now = DateTime.Now;
+            DateTime from = new DateTime(now.Year, now.Month, 1);
+            DateTime to = new DateTime(now.Year, now.Month, DateTime.DaysInMonth(now.Year, now.Month));
+
+            return Index(from.ToString(), to.ToString());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string fromDate, string toDate)
+        {
+            DateTime from = DateTime.Parse(fromDate);
+            DateTime to = DateTime.Parse(toDate).AddDays(1);
+
+            ViewData["MonthlyReport"] = Models.Data.Reporting.MonthlyReport(from, to);
+            ViewData["ProblemReport"] = Models.Data.Reporting.ProblemReport();
             return View();
         }
     }
